73d43ffdeb5eecefbd45d0caf72769067079e5f8
Jordan Taylor <nadrojtayl@hotmail.com>
2020-06-17 20:50:37 -0400

    more

--- App.js
+++ App.js
@@ -1,12 +1,73 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+
+
+function try_eval(input){
+  try {
+    var output =  eval(input);
+    return output
+  } catch(e){
+    return input;
+
+  }
+ }
+
+
+function filter(arr,phrase){
+  return arr.filter(function(elem){
+    return elem.indexOf(phrase) !== -1;
+  })
+}
+
+function filter_list_of_objs(arr,key,filter_value){
+  return arr.filter(function(obj){
+    return obj[key] === filter_value
+  })
+}
+
+function map_list_of_objs(arr,key){
+  return arr.map(function(obj){
+    return obj[key];
+  })
+}
+
+function clone(arr){
+  return arr.slice();
+}
+
+
+function convert_spreadsheet_data_to_obj(data){
+  return {
+    row: data.gs$cell.row,
+    col: data.gs$cell.col,
+    data: data.content.$t,
+    type:data.content.type
+  }
+}
+
+
+
+function unwrap_dynamically(value,default_value){
+  if(default_value === undefined){
+    default_value = "undefined"
+  }
+  return try_eval(value) === undefined ? (default_value):  try_eval(value) 
+}
+
+class App extends React.Component {
+  
+  constructor(props){
+    super(props);
+  }
+
+  render(){
+    return (
+      <View style={styles.container}>
+        <Text>Open up App.js to start working on your app!</Text>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
@@ -17,3 +78,5 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+export default App;
