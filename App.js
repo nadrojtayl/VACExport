2b067099c773eb4c7303b030ee449f8b56de47be
Jordan Taylor <nadrojtayl@hotmail.com>
2020-06-17 21:32:02 -0400

    more

--- App.js
+++ App.js
@@ -1,7 +1,155 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, ScrollView, TouchableOpacity, Button, Picker, Switch, Image, Text, View } from 'react-native';
 
 
+
+  class Multiplier extends React.Component{
+    constructor(props){
+      super(props);
+      this.state = {
+        childrenAdditionalStyles: [],
+        clickfunctions: [],
+        textTreeChildren:[]
+      }
+    }
+
+  renderElement(name,int, additionalStyle, clickfunctions,elem){
+    var that = this;
+    console.log(additionalStyle)
+
+    var copy = {};
+    additionalStyle.forEach(function(obj,ind){
+      // console.log(obj);
+      Object.keys(obj).forEach(function(key){
+   
+        if(key !== undefined && key.indexOf("repeater") !== -1){
+          copy[key.replace("repeater","")] = obj[key];
+        }
+
+      })
+    })
+    
+
+    additionalStyle = copy;
+
+    Object.keys(additionalStyle).forEach(function(key){
+      if(key !== "onPress" && typeof additionalStyle[key] === "string" && (additionalStyle[key].indexOf('elem') !== -1  || additionalStyle[key].indexOf('width') !== -1 || additionalStyle[key].indexOf('height') !== -1 ) ){
+        additionalStyle[key] = eval(additionalStyle[key])
+      }
+    })
+
+
+
+    var innerText = additionalStyle.innerText;
+  
+
+     try {
+     var evaled = eval(innerText)
+     innerText= evaled;
+    } catch(e){
+
+    }
+
+    console.log("STYLE");
+    console.log(additionalStyle)
+
+
+    int = parseInt(int)
+    if(name === "text"){
+
+
+      return (
+        <Text
+          style={[{ height: 40, borderColor: 'black', backgroundColor:'white', color:'black', width:"100%", borderWidth: 5}, additionalStyle]}
+          key = {int}
+          onPress = { function(){  eval('(' + that.props.clickfunction + ')()')   } }
+          selectable = {true}
+        >{  additionalStyle.innerText === undefined ? "example" :additionalStyle.innerText }</Text>
+
+        )
+    }
+
+
+    if(name === "button"){
+    
+      return(
+        <TouchableOpacity
+          onPress = { function(){ eval(additionalStyle.onPress) } } 
+          key = {int}
+          style={[{
+            shadowOffset: { height: 1, width: 1 }, // IOS
+            shadowOpacity: 1, // IOS
+            shadowRadius: 1, //IOS
+            backgroundColor: 'white',
+            color:'black',
+            elevation: 2, // Android
+            marginTop:"5%",
+            justifyContent: 'center',
+            alignItems: 'center',
+            flexDirection: 'row',
+            height:"7%",
+            borderRadius: 10,
+            borderColor: 'gray', borderWidth: 1}, additionalStyle]}
+        ><Text style = {{textAlign:'center'}}> { unwrap_dynamically(additionalStyle['innerText'])  }</Text>
+        </TouchableOpacity>
+      )
+    }
+
+    if(name === "image"){
+      return(
+       <Image
+          source = {{uri: additionalStyle['source'] === undefined ? ("https://i.imgur.com/89iERyb.png"):additionalStyle['source']}}
+          key = {int}
+          style={[{ width:'200px', height:'200px'}, additionalStyle]}
+        ></Image>
+     
+      )
+    }
+
+
+
+
+  }
+
+
+    render(){
+
+      var that = this;
+  
+    
+
+      if(!window.edit_mode){
+        return (<View
+          style = {that.props.style}
+          >
+       <ScrollView>
+          {that.props.data.map(function(elem,ind){
+
+            return that.renderElement(that.props.type,ind,that.props.style, that.props.clickfunction, elem)
+          }) }
+        </ScrollView>
+        </View>
+        )
+
+
+      }
+
+      return (<TouchableOpacity
+      style = {that.props.style}
+      onPress = { function(){if(window.drag_mode){ that.setState({selectedElemToStyle:that.props.int});  return} if(window.edit_mode){ console.log("IND" + that.props.int); window.edit(that.props.int); return}  eval('(' + that.state.clickfunctions[that.props.int] + ')()'); if(that.state.clickfunctions[that.props.int].indexOf("appData") !== -1){ that.forceUpdate()}   } }
+
+      >
+        <ScrollView>
+          {that.props.data.map(function(elem,ind){
+            console.log(that.props.clickfunction)
+            return that.renderElement(that.props.type,ind,that.props.style, that.props.clickfunction, elem)
+          }) }
+        </ScrollView>
+        </TouchableOpacity>
+        )
+    }
+
+  }
 
 function try_eval(input){
   try {
@@ -55,19 +203,59 @@ function unwrap_dynamically(value,default_value){
   return try_eval(value) === undefined ? (default_value):  try_eval(value) 
 }
 
+
+//prettifier: https://www.prettifier.net/js/
 class App extends React.Component {
-  
-  constructor(props){
-    super(props);
+
+constructor(props){
+              super(props);
+              this.state = {"key":"value","list":[1,2,3,4]}
+            }
+
+            render(){ var appData = this.state; var that = this; return (
+              <View style = {{width:"380px", height:"657px", backgroundColor:"red"}}> 
+              <TouchableOpacity
+          onPress = { function(){appData.key = "more"; that.forceUpdate(); }}  
+          style= {[{
+            shadowColor: 'rgba(0,0,0, .4)', // IOS
+            shadowOffset: { height: 1, width: 1 }, // IOS
+            shadowOpacity: 1, // IOS
+            shadowRadius: 1, //IOS
+            backgroundColor: '#fff',
+            elevation: 2, // Android
+            height: 50,
+            width: 100,
+            justifyContent: 'center',
+            alignItems: 'center',
+            flexDirection: 'row',
+            position:'absolute',top:0,left:0, backgroundColor:'#8fd158', alignItems:'center',justifyContent:'center', height: "7%",  title:'Test', borderColor: 'gray', color:'black', borderRadius:15, borderWidth: 1},{"top":214.09247999999997,"left":195.29751499999998,"innerText":"Click"}]}
+        >
+        <Text>
+        {"Click"}
+        </Text>
+        </TouchableOpacity>
+        <Text
+          style= {{"top":72.09247999999997,"left":200.29751499999998,"innerText":"appData.key"}}
+        > {appData.key}
+         </Text>
+        
+<Multiplier
+      type = {"text"}
+      data = {appData.list}
+      style = {[{alignItems:'center'},{"options":"appData.list","top":326.09247999999997,"left":-23.702485000000024,"repeaterinnerText":"elem"}]}
+     
+      >
+      </Multiplier>
+      </View>)}
+
+
+  goTo(pageName){
+    this.setState({page:pageName,
+      children:this.state.pages[pageName].children,
+      childrenAdditionalStyles: this.state.pages[pageName].childrenAdditionalStyles,
+      clickfunctions: this.state.pages[pageName].clickfunctions })
   }
 
-  render(){
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
-    );
-  }
 }
 
 const styles = StyleSheet.create({
