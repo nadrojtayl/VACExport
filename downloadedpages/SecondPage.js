6d80803e0d2bb4e2fcdc9246a1ccade32b1587dd
Jordan Taylor <nadrojtayl@hotmail.com>
2020-06-28 00:33:37 -0400

    progress

--- downloadedpages/SecondPage.js
+++ downloadedpages/SecondPage.js
@@ -4,6 +4,60 @@ import appData from './global.js';
 
 
 
+function try_eval(input){
+  try {
+    var output =  eval(input);
+    return output
+  } catch(e){
+    return input;
+
+  }
+ }
+
+
+function filter(arr,phrase){
+  return arr.filter(function(elem){
+    return elem.indexOf(phrase) !== -1;
+  })
+}
+
+function filter_list_of_objs(arr,key,filter_value){
+  return arr.filter(function(obj){
+    return obj[key] === filter_value
+  })
+}
+
+function map_list_of_objs(arr,key){
+  return arr.map(function(obj){
+    return obj[key];
+  })
+}
+
+function clone(arr){
+  return arr.slice();
+}
+
+
+function convert_spreadsheet_data_to_obj(data){
+  return {
+    row: data.gs$cell.row,
+    col: data.gs$cell.col,
+    data: data.content.$t,
+    type:data.content.type
+  }
+}
+
+
+
+function unwrap_dynamically(value,default_value){
+  if(default_value === undefined){
+    default_value = "undefined"
+  }
+  return try_eval(value) === undefined ? (default_value):  try_eval(value) 
+}
+
+
+
   class Multiplier extends React.Component{
     constructor(props){
       super(props);
@@ -160,16 +214,6 @@ import appData from './global.js';
         this.state = {};
       }
 
-      componentDidMount(){
-       
-        var that = this;
-        var dbLinks = {"weapons":"https://spreadsheets.google.com/feeds/cells/1QQnDBXvWjKs7wl0OjHlpp72eR-bfdtz9X0eAtuFeo6o/1/public/full?alt=json"}
-        Object.keys(dbLinks).forEach(function(key){
-          that.connectToDatabase(dbLinks[key], key);
-        })
-
-
-      }
       render(){ 
       var that = this; 
       
