3fab2022647fdf7a2aae5532222bf5c7a3cd93fb
Jordan Taylor <nadrojtayl@hotmail.com>
2020-06-28 20:10:43 -0400

    More

--- downloadedpages/RegisterPage.js
+++ downloadedpages/RegisterPage.js
@@ -68,31 +68,28 @@ class RegisterPage extends React.Component {
 
 
       return (
-      <View style = {{width:"100%", height:"100%", borderWidth:5, borderColor:"black", backgroundColor:"white"}}><TextInput
-       style= {{"top":243.712,"left":3.8204999999999814,"width":"90%"}}
-        value={that.state["RegisterPageinput0"]}
-        onChangeText={function(val){that.setState({RegisterPageinput0: val});  
-       }
-       }
-        />
-<Text
-          style= {{"top":-28.28800000000001,"left":47.82049999999998,"innerText":null}}
-        > {null} </Text>
+      <View style = {{width:"100%", height:"100%", borderWidth:5, borderColor:"black", backgroundColor:"white"}}>
+      
+
         
-<Text
-          style= {{"top":213.712,"left":-0.17950000000001864,"innerText":"What Type of Animal","color":"black","backgroundColor":"#9a82cf","textAlign":null}}
+        <Text
+          style= {{textAlign:'center', marginTop:"25%", alignItems:'center', "left":"0%","innerText":"What Type of Animal","color":"black","backgroundColor":"#9a82cf","textAlign":null}}
         > {"What Type of Animal"} </Text>
+        <TextInput
+         style= {{"width":"90%"}}
+          value={that.state["RegisterPageinput0"]}
+          onChangeText={function(val){that.setState({RegisterPageinput0: val});  
+         }
+         }
+          />
         
-<Text
-          style= {{"top":34.71199999999999,"left":37.82049999999998,"innerText":null}}
-        > {null} </Text>
         
-<Text
-          style= {{"top":290.712,"left":-0.17950000000001864,"innerText":"What Breed of Animal","backgroundColor":"#9a82cf"}}
+        <Text
+          style= {{"innerText":"What Breed of Animal","backgroundColor":"#9a82cf"}}
         > {"What Breed of Animal"} </Text>
         
 <TextInput
-       style= {{"top":327.712,"left":2.8204999999999814,"width":"90%"}}
+       style= {{"width":"90%"}}
         value={that.state["RegisterPageinput5"]}
         onChangeText={function(val){that.setState({RegisterPageinput5: val});  
        }
@@ -117,11 +114,11 @@ class RegisterPage extends React.Component {
         ><Text>{"< Back"}</Text>
         </TouchableOpacity>
 <Text
-          style= {{"top":380.712,"left":0.8204999999999814,"innerText":"Animal Description","backgroundColor":"#9a82cf"}}
+          style= {{"innerText":"Animal Description","backgroundColor":"#9a82cf"}}
         > {"Animal Description"} </Text>
         
 <TextInput
-       style= {{"top":410.712,"left":3.8204999999999814,"alignItems":null,"width":"90%"}}
+       style= {{"alignItems":null,"width":"90%"}}
         value={that.state["RegisterPageinput8"]}
         onChangeText={function(val){that.setState({RegisterPageinput8: val});  
        }
@@ -155,16 +152,17 @@ class RegisterPage extends React.Component {
             position:'absolute',top:0,left:0, backgroundColor:'#8fd158', alignItems:'center',justifyContent:'center', height: "7%",  title:'Test', borderColor: 'gray', color:'black', borderRadius:15, borderWidth: 1},{"top":475.712,"left":96.82049999999998,"innerText":"Done","width":"50%","textAlign":"center","backgroundColor":"#a3e3a9","color":"black"}]}
         ><Text>{"Done"}</Text>
         </TouchableOpacity>
-<TextInput
-       style= {{"top":166.712,"left":3.8204999999999814,"width":"90%"}}
+        <Text
+          style= {{"innerText":"Enter Email/Phone","backgroundColor":"#9a82cf"}}
+        > {"Enter Email/Phone"} </Text>
+      <TextInput
+       style= {{"width":"90%"}}
         value={that.state["RegisterPageinput10"]}
         onChangeText={function(val){that.setState({RegisterPageinput10: val});  
        }
        }
         />
-<Text
-          style= {{"top":137.712,"left":-0.17950000000001864,"innerText":"Enter Email/Phone","backgroundColor":"#9a82cf"}}
-        > {"Enter Email/Phone"} </Text>
+
         </View>
         )
       }
