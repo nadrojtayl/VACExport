cf7db9f4f9dd7a7180cb78f6f865521116e4e34d
Jordan Taylor <nadrojtayl@hotmail.com>
2020-06-30 20:21:54 -0400

    more

--- downloadedpages/FirstPage.js
+++ downloadedpages/FirstPage.js
@@ -2,7 +2,9 @@ import React from 'react';
 import { TextInput, StyleSheet, ScrollView, TouchableOpacity, Button, Picker, Switch, Image, Text, View } from 'react-native';
 import appData from './global.js';
 
-
+function onlyUnique(value, index, self) { 
+    return self.indexOf(value) === index;
+}
 
 function try_eval(input){
   try {
@@ -144,12 +146,12 @@ function unwrap_dynamically(value,default_value){
             backgroundColor: 'white',
             color:'black',
             elevation: 2, // Android
-            marginTop:"5%",
             justifyContent: 'center',
             alignItems: 'center',
             flexDirection: 'row',
-            height:"7%",
+            height:"20%",
             borderRadius: 10,
+            marginTop:10,
             borderColor: 'gray', borderWidth: 1}, additionalStyle]}
         ><Text style = {{textAlign:'center'}}> { unwrap_dynamically(additionalStyle['innerText'])  }</Text>
         </TouchableOpacity>
@@ -161,7 +163,7 @@ function unwrap_dynamically(value,default_value){
        <Image
           source = {{uri: additionalStyle['source'] === undefined ? ("https://i.imgur.com/89iERyb.png"):additionalStyle['source']}}
           key = {int}
-          style={[{ width:'200px', height:'200px'}, additionalStyle]}
+          style={[{ width:'20%', height:'20%'}, additionalStyle]}
         ></Image>
      
       )
@@ -234,28 +236,31 @@ class FirstPage extends React.Component {
 
 
       return (
-      <View style = {{width:"380px", height:"657px", borderWidth:5, borderColor:"black", backgroundColor:"white"}}><Text
-          style= {{"innerText":"'Slippery's Guide to a Fortnite Tier List'","textAlign":"center","top":64.09247999999997,"left":53.297514999999976,"width":"75%","height":"7%","backgroundColor":"#49c4fc","fontSize":24}}
-        > {"Slippery's Guide to a Fortnite Tier List"} </Text>
+      <View style = {{width:"100%", height:"100%", borderWidth:5, borderColor:"black", backgroundColor:"white"}}>
+      <Text
+          style= {{"innerText":"'Slippery's Guide to a Fortnite Tier List'","textAlign":"center","top":64.09247999999997,"left":0,"width":"100%","height":"7%","backgroundColor":"#49c4fc","fontSize":24}}
+        > {"Slippery's Fortnite Tier List"} </Text>
         
-<Text
-          style= {{"top":340.904,"left":4.813500000000033,"innerText":"'Click a Class to View'","textAlign":"center"}}
+      <Text
+          style= {{"top":"40%","left":4.813500000000033,"innerText":"'Click a Class to Improve Your Game'","textAlign":"center"}}
         > {'Click a Class to View'} </Text>
+        <Image
+        style= {[{width:"50%",height:"20%"}, {"source":"https://cm1.narvii.com/7192/f75cb8c8074b5ccc961668aa91bbec9256a4c544_00.jpg","top":"10%",
+        "left":"25%","height":"25%"}]}
+        source = {{uri:"https://cm1.narvii.com/7192/f75cb8c8074b5ccc961668aa91bbec9256a4c544_00.jpg"}}
+      >
+      </Image>
         
 <Multiplier
       type = {"button"}
       goTo = {that.props.goTo}
-      data = {map_list_of_objs(weapons,"Gun Class")}
-      style = {[{alignItems:'center'},{"options":"map_list_of_objs(weapons,\"Gun Class\")","repeaterinnerText":"elem","repeaterType":"button","top":398.904,"left":-9.186499999999967,"width":null,"height":"40%","repeateronPress":"appData.SelectedGroup = elem;\nappData.FilteredList = filter_list_of_objs(weapons,\"Gun Class\",elem);\ngoTo(\"SecondPage\");"}]}
+      data = { map_list_of_objs(weapons,"Gun Class").filter(onlyUnique) }
+      style = {[{alignItems:'center'},{"options":"map_list_of_objs(weapons,\"Gun Class\")","repeaterinnerText":"elem","repeaterType":"button","top":"20%","left":-9.186499999999967,"width":null,"height":"40%","repeateronPress":"appData.SelectedGroup = elem;\nappData.FilteredList = filter_list_of_objs(weapons,\"Gun Class\",elem);\ngoTo(\"SecondPage\");"}]}
       clickfunction = {function(){goTo("SecondPage");}}
       >
       </Multiplier>
 
-      <Image
-        style= {[{width:"200px",height:"200px"}, {"source":"https://cm1.narvii.com/7192/f75cb8c8074b5ccc961668aa91bbec9256a4c544_00.jpg","top":-87.24799999999999,"left":109.91700000000003,"height":"25%"}]}
-        source = {{uri:"https://cm1.narvii.com/7192/f75cb8c8074b5ccc961668aa91bbec9256a4c544_00.jpg"}}
-      >
-      </Image>
+
  <TouchableOpacity
           
           onPress = { function(){that.props.goTo("Privacy"); that.forceUpdate(); }}  
---
+++ ios/Pods/Target Support Files/Pods-appbuilderexporttest/Pods-appbuilderexporttest-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_appbuilderexporttest : NSObject
+@end
+@implementation PodsDummy_Pods_appbuilderexporttest
+@end
