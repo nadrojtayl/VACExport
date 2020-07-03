294dbba1ab5e4e6328818fd85906f86632fa6b2b
Jordan Taylor <nadrojtayl@hotmail.com>
2020-07-02 21:57:48 -0400

    More

--- downloadedpages/global.js
+++ downloadedpages/global.js
@@ -1,2 +1,10 @@
-var appData = {matchingList:[], AddorFind:true,"key":"value","RegisterPageinput0":"Dog","RegisterPageinput5":"Russian Blue","RegisterPageinput8":"Light blue skin","Info Pageinput1":"d","RegisterPageinput10":"973 768 9820","RegisterPageinput11":"","FindorAdd":true,"AddorFind":true,"loaded":false,"dbLinks":{}}
+var appData = {matchingList:[], AddorFind:true,
+	"key":"value",
+	"RegisterPageinput0":"Animal Type Here",
+	"RegisterPageinput5":"Type Breed Here",
+	"RegisterPageinput8":"Write Description here",
+	"Info Pageinput1":"d",
+	"RegisterPageinput10":"xxx-xxx-xxxx",
+	"RegisterPageinput11":"",
+	"FindorAdd":true,"AddorFind":true,"loaded":false,"dbLinks":{}}
 export default appData;
