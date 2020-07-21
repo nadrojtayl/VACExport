568c750db9c70b868ee32929d0de8afa3ba680a8
Jordan Taylor <nadrojtayl@hotmail.com>
2020-07-20 20:37:17 -0400

    More

--- index.js
+++ index.js
@@ -6,11 +6,28 @@ var fs = require('fs');
 var cors = require('cors');
 app.use(cors());
 
+const { exec } = require("child_process");
+
 
 
 app.get("/",function(req,res){
 	res.send("HERE");
+	exec("node downloadvac.js Baptiste-Louis FlagRunGame flagrung.flagrung.com; expo publish",{
+	  cwd: __dirname + "/appbuilderexporttest"
+	}, (error, stdout, stderr) => {
+		    if (error) {
+		        console.log(`error: ${error.message}`);
+		        return;
+		    }
+		    if (stderr) {
+		        console.log(`stderr: ${stderr}`);
+		        return;
+		    }
+		    console.log(`stdout: ${stdout}`);
+		});
+
+
 })
 
-
-app.listen(process.env.PORT || 5000);
+// console.log("Listening");
+// app.listen(process.env.PORT || 5000);
