1534cdbeced9c71b437518d861b5a6b5042b8b27
Jordan Taylor <nadrojtayl@hotmail.com>
2020-08-10 00:14:01 -0400

    submitted

---
+++ appbuilderexporttest/downloadedpages/IncorrectPage.js
@@ -0,0 +1,326 @@
+
+import React, { Component } from "react";
+import { ActivityIndicator, Button, Picker, Switch, Image, ScrollView, TouchableOpacity, StyleSheet, Text, View, TextInput, Dimensions } from "react-native";
+import Calendar from "./Calendar.js";
+import appData from "./global.js";
+import Multiplier from "./Multiplier.js";
+import { Audio } from 'expo-av'; 
+
+
+var d = new Date();
+var month = d.getMonth();
+var day = d.getDate();
+global.month = d.getMonth();
+global.day = d.getDate();
+global.created = [];
+
+function hasNumber(myString) {
+  return /d/.test(myString);
+}
+
+
+
+const width = Dimensions.get('window').width;
+const height = Dimensions.get('window').height;
+
+function resizeFont(num){
+  return num / (640/height)
+}
+
+
+global.inputs = {
+  0:"This"
+}
+
+class Box extends React.Component{
+  render(){
+    return (<View style={this.props.view}></View>)
+  }
+}
+
+
+
+
+//READ ONLY API: format
+//https://spreadsheets.google.com/feeds/cells/1P0tGuikrAg5ZGpC2fHxLY49Osp6nhwseK2DSr34HM-o/1/public/full?alt=json
+
+function runWithInterval(script_string,interval){
+  var script_string = script_string + "; global.thisapp.forceUpdate();"
+  script_string = script_string.split("createElement").join("global.thisapp.createElement");
+  var that = appData.this;
+    try{
+        eval("function y(){"+script_string+"}")
+        global.thisapp.forceUpdate();
+        return setInterval(function(){ eval(script_string)},interval);
+      } catch(e){
+        alert("There was an error trying to run this!" + e);
+      }
+}
+
+window.runWithInterval = runWithInterval;
+runWithInterval = runWithInterval;
+
+function realEval(str){
+  return eval(str);
+}
+
+window.realEval = realEval.bind(this);
+
+function try_eval(input){
+  try {
+    var output =  eval(input);
+    return output
+  } catch(e){
+    return input;
+
+  }
+ }
+
+ function filter_obj_by_phrase(arr,key_name,phrase){
+  return arr.filter(function(elem){
+    return elem[key_name].indexOf(phrase) !== -1;
+  })
+}
+
+
+
+function filter(arr,phrase){
+  return arr.filter(function(elem){
+    return elem.indexOf(phrase) !== -1;
+  })
+}
+
+window.filter = filter;
+
+function onlyUnique(value, index, self) { 
+    return self.indexOf(value) === index;
+}
+
+window.onlyUnique = onlyUnique;
+
+function unique(arr){
+  return arr.filter(onlyUnique);
+}
+
+window.unique = unique;
+
+function unique_return_one_key(arr,key){
+  return arr.map(function(obj){
+    return obj[key]
+  }).filter(onlyUnique);
+}
+
+function findInArrayOfObjects(array,search_obj) {
+          
+            for(var i = 0; i < array.length;i++){
+              var match = true;
+
+              Object.keys(search_obj).forEach(function(key){
+                if(search_obj[key] !== array[i][key]){
+                  match = false;
+                }
+              })
+
+              if(match){
+                return array[i];
+              }
+
+            }
+            return false;
+        }
+
+  window.findInArrayOfObjects = findInArrayOfObjects;
+
+window.unique_return_one_key = unique_return_one_key;
+
+function filter_list_of_objs(arr,key,filter_value){
+  return arr.filter(function(obj){
+    return obj[key] === filter_value
+  })
+}
+
+window.filter_list_of_objs = filter_list_of_objs;
+
+function filter_list_of_objs_multiple_keys(arr,filter_object){
+  return arr.filter(function(obj){
+    var match = true;
+    Object.keys(filter_object).forEach(function(key){
+      if(filter_object[key] !== obj[key]){
+        match = false;
+      }
+    })
+    return match
+  })
+}
+
+global.filter_list_of_objs_multiple_keys = filter_list_of_objs_multiple_keys;
+
+
+function map_list_of_objs(arr,key){
+  return arr.map(function(obj){
+    return obj[key];
+  })
+}
+
+global.map_list_of_objs = map_list_of_objs;
+
+function clone(arr){
+  return arr.slice();
+}
+
+global.audio = [];
+async function play(url){
+  appData.soundObject = new Audio.Sound();
+ try {
+    await appData.soundObject.loadAsync({uri:"https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3"});
+    await appData.soundObject.playAsync();
+    // Your sound is playing!
+  } catch (error) {
+    alert(error);
+  }
+
+
+}
+
+async function pause(){
+   await appData.soundObject.pauseAsync();
+}
+
+global.play = play;
+global.pause = pause;
+
+global.clone = clone;
+
+function convert_spreadsheet_data_to_obj(data){
+  return {
+    row: data.gs$cell.row,
+    col: data.gs$cell.col,
+    data: data.content.$t,
+    type:data.content.type
+  }
+}
+
+
+
+function unwrap_dynamically(value,default_value){
+  if(default_value === undefined){
+    default_value = "undefined"
+  }
+  var value = (try_eval(value) === undefined) ? (default_value):  try_eval(value);
+  if(typeof value === "object"){return "Error: This is an object"}
+  return value;
+}
+
+
+
+
+ class IncorrectPage extends React.Component {
+     
+
+    constructor(props)
+    {
+        super(props);
+        this.state = {"counter":4,"coins":0,"SecondChance":1,"visible":0,"createdelems":[]}
+    }
+
+    componentDidMount(){
+      global.thisapp = this;
+    }
+
+
+      
+  
+    createElement(name, style_obj){
+      if(name === 'image'){
+        this.state.createdelems.push(
+        (<Image style = {[{position:'absolute', width:"20%",height:"20%"}, style_obj]}></Image>)
+        )
+      }
+
+       if(name === 'text'){
+        this.state.createdelems.push(
+        (<Text style = {style_obj}></Text>)
+        )
+      }
+
+      global.created.push(style_obj);
+     
+
+
+
+    }
+
+   
+
+    render(){ 
+      var that = this; 
+      appData.this = this;
+      
+      if(!that.props.loaded){
+        return(<View style = {{height:'100%',width:'100%', alignItems:'center',justifyContent:'center'}}>
+        <ActivityIndicator size="large" />
+        <Text style = {{textAlign:'center'}}>Are you a student? Build an app with VineyardAppCamp.com</Text>
+        </View>)
+      }
+      return (
+      <View style = {{width:"100%", height:"100%", borderWidth:5, borderColor:"black", backgroundColor:"white"}}>
+      {this.state.createdelems}
+
+       <TouchableOpacity
+          
+          onPress = { function(){that.props.goTo("FirstPage")
+appData.visible=0
+if(appData.counter>9){
+    appData.counter=0
+}; that.forceUpdate(); }}  
+          style= {[{
+            shadowColor: 'rgba(0,0,0, .4)', // IOS
+            shadowOffset: { height: 1, width: 1 }, // IOS
+            shadowOpacity: 1, // IOS
+            shadowRadius: 1, //IOS
+            backgroundColor: '#fff',
+            elevation: 2, // Android
+            justifyContent: 'center',
+            alignItems: 'center',
+            flexDirection: 'row',
+            height:"7%",
+            width:"30%",
+            position:'absolute',top:0,left:0, 
+            backgroundColor:'#8fd158',
+             alignItems:'center',
+             justifyContent:'center', height: "7%",  
+             title:'Test', borderColor: 'gray', color:'black',
+              borderRadius:15, borderWidth: 1},
+              {"top":"83.79%","left":"66.7%","innerText":"'Next'","backgroundColor":"yellow"}]}
+        >
+        <Text style = {{color:"black"}}>
+
+        {'Next'}
+
+       </Text>
+        </TouchableOpacity>
+<Text
+          style= {[{position:'absolute',zIndex:100,width:'100%'},{"top":"74.02%","left":"0.59%","innerText":"'Whoops So Close!!!!'","fontSize":resizeFont(30),"color":"red","backgroundColor":"white"}]}
+        > {'Whoops So Close!!!!'} </Text>
+        
+
+
+      <Image
+        style= {[{width:"20%",height:"20%",position:'absolute'}, {"source":"https://pbs.twimg.com/profile_images/3292148099/9dd84e51596afa0bf4b576673f7153d1_400x400.jpeg","width":"100%","top":"-6.08%","left":"-0.12%","height":"80%"}]}
+        source = {{uri:'https://pbs.twimg.com/profile_images/3292148099/9dd84e51596afa0bf4b576673f7153d1_400x400.jpeg'}}
+        onPress = { function(){; that.forceUpdate(); }}  
+      >
+      </Image>
+
+
+
+      
+        </View>
+        )
+    }
+  }
+    export default IncorrectPage; 
+
+
+
+  
---
+++ appbuilderexporttest/ios/Pods/Target Support Files/Pods-WhenAreSportsBack/Pods-WhenAreSportsBack-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_WhenAreSportsBack : NSObject
+@end
+@implementation PodsDummy_Pods_WhenAreSportsBack
+@end
